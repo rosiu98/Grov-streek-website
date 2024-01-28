17a60c3b38ef90534020d3457937c42df8fcf55a
Pawel Rosiek <rosiu978@gmail.com>
2024-01-28 01:17:06 +0100

    first version of scrapper

---
+++ app/api/data/route.ts
@@ -0,0 +1,145 @@
+import { supabase } from "@/libs/supabase";
+import type { NextApiRequest, NextApiResponse } from "next";
+import puppeteer from "puppeteer-core";
+
+type Json = {
+  message: string;
+};
+
+interface ScrapData {
+  imageLogo: string;
+  Position: string;
+  teamName: string;
+  teamCode: string;
+  teamUrl: string;
+  GP: string;
+  W: string;
+  L: string;
+  Pts: string;
+  "%won": string;
+  For: string;
+  Agst: string;
+  GD: string;
+  Str: string;
+  L5: string;
+}
+
+export const GET = async (
+  req: NextApiRequest,
+  res: NextApiResponse<Json | Buffer>
+) => {
+  try {
+    const url =
+      "https://hosted.dcd.shared.geniussports.com/LMBA/en/competition/36602/standings?phaseName=Division%20IV&";
+
+    const browser = await puppeteer.connect({
+      browserWSEndpoint: `wss://chrome.browserless.io?token=${process.env.BLESS_TOKEN}`,
+    });
+
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    // Wait for some time or use other strategies to ensure content is loaded
+    await page.waitForTimeout(3000);
+
+    const data = await page.evaluate(() => {
+      const table = document.querySelector(".table-wrap");
+      if (!table) {
+        throw new Error("Table not found on the page");
+      }
+
+      // Extract data from the table
+      const rows = Array.from(table.querySelectorAll("tr")).slice(1); // Skip header row
+      const headers = Array.from(table.querySelectorAll("th")).map((th) =>
+        th.textContent?.trim()
+      );
+
+      const result = rows.map((row) => {
+        const cells = Array.from(row.querySelectorAll("td")).map((td) =>
+          td.textContent?.trim()
+        ) as [string];
+
+        if (!cells || !headers) return;
+
+        // Extract image URL from the "Team" column (assuming it's an img element)
+        const teamLogoSrc =
+          row.querySelector(".team-logo img")?.getAttribute("src") || null;
+
+        // Change "L5" column to an array
+        const l5Array = cells[headers.indexOf("L5")].split(/\s+/);
+
+        // Extract team name, team code, and team URL
+        const teamInfoElement = row.querySelector(".team-name a");
+        const teamName =
+          teamInfoElement
+            ?.querySelector(".team-name-full")
+            ?.textContent?.trim() || null;
+        const teamCode =
+          teamInfoElement
+            ?.querySelector(".team-name-code")
+            ?.textContent?.trim() || null;
+        const teamUrl = teamInfoElement?.getAttribute("href") || null;
+
+        const obj: any = {
+          imageLogo: teamLogoSrc,
+          teamName,
+          teamCode,
+          teamUrl,
+        };
+
+        headers.forEach((header, index) => {
+          if (!header) return;
+
+          if (header === "L5") {
+            obj[header] = l5Array;
+          } else {
+            obj[header] = cells[index];
+          }
+        });
+
+        return obj;
+      });
+
+      return result as ScrapData[];
+    });
+
+    if (data) {
+      await supabase.from("lista_wynikow").delete().gte("id", 0);
+    }
+
+    const parseData = data.map((item) => {
+      return {
+        position: item.Position,
+        teamName: item.teamName,
+        teamCode: item.teamCode,
+        teamUrl: item.teamUrl,
+        gp: Number(item.GP),
+        wins: Number(item.W),
+        loses: Number(item.L),
+        points: Number(item.Pts),
+        winration: item["%won"],
+        for: Number(item.For),
+        agst: Number(item.Agst),
+        gd: Number(item.GD),
+        str: Number(item.Str),
+        lastMatchesArray: item.L5,
+        teamLogo: item.imageLogo,
+      };
+    });
+
+    const { data: supabaseData } = await supabase
+      .from("lista_wynikow")
+      .insert(parseData)
+      .select("*");
+
+    await browser.close();
+
+    return new Response(JSON.stringify(supabaseData), { status: 200 });
+  } catch (error: any) {
+    console.error("Scraping failed:", error.message);
+    return new Response(
+      JSON.stringify({ message: "Scraping failed", error: error.message }),
+      { status: 200 }
+    );
+  }
+};

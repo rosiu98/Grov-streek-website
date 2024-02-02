d4e86a8c4e63d202e23c22aee7292f909ab1c682
Pawel Rosiek <rosiu978@gmail.com>
2024-02-02 18:22:24 +0100

    adding mobile classes fixes

--- app/ui/main.tsx
+++ app/ui/main.tsx
@@ -3,7 +3,7 @@ import backgroundImage from "@/public/bg_header.jpg";
 const Main = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="h-full text-white relative">
-      <div className="h-[1000px]">{children}</div>
+      <div className="min-h-[1000px] h-full">{children}</div>
     </main>
   );
 };
--- app/ui/news.tsx
+++ app/ui/news.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 
 const News = () => {
   return (
-    <section className="pt-[190px]">
+    <section className="pt-[100px] lg:pt-[190px] pb-10">
       <div className="max-w-app">
         <div className="mb-12">
           <p className="text-white text-3xl font-extrabold mb-7">AKTUALNOŚCI</p>
@@ -16,7 +16,7 @@ const News = () => {
         <div className="grid grid-cols-1 h-auto lg:h-[480] xl:grid-cols-[auto_1fr] text-left gap-7">
           <Link className="articleLink" href="/">
             <div className="relative">
-              <div className="absolute z-20 top-0 pt-5 pl-10">
+              <div className="absolute z-20 top-0 pt-5 pl-5 md:pl-10">
                 <div className="flex items-center justify-center gap-1 bg-black p-1 rounded-md">
                   <span>
                     <TimeIconSvg />
@@ -25,8 +25,8 @@ const News = () => {
                 </div>
               </div>
 
-              <div className="absolute z-20 bottom-0 pb-10 pl-10">
-                <h3 className="text-white text-3xl font-extrabold">
+              <div className="absolute z-20 bottom-0 pb-5 pl-5 md:pb-10 md:pl-10">
+                <h3 className="text-white text-lg md:text-3xl font-extrabold">
                   Sukcesy Grove Street Ballers: Odkrywamy Twarze&nbsp;Mistrzów!
                 </h3>
               </div>
@@ -43,7 +43,7 @@ const News = () => {
           </Link>
 
           <div className="flex gap-7 flex-col">
-            <div className="grid grid-cols-[auto_1fr] h-full">
+            <div className="grid grid-cols-[0.4fr_0.6fr] md:grid-cols-[auto_1fr] h-full">
               <Link href="/" className="articleLink h-full">
                 <div className="article_secondImage relative h-full">
                   <Image
@@ -62,18 +62,18 @@ const News = () => {
                   </span>
                   <span className="text-[12px]">28.12.2023</span>
                 </div>
-                <div className="text-[#31404b]  mt-4 font-bold text-lg">
+                <div className="text-[#31404b]  mt-4 mb-4 font-bold text-sm sm:text-lg ">
                   Czasem spojrzenie mówi więcej niż setki słów
                 </div>
                 <div className="mt-auto">
-                  <div className="bg-[#272727] px-4 py-2 font-bold rounded-md">
+                  <div className="bg-[#272727] px-4 py-2 font-bold rounded-md text-sm md:text-base">
                     <Link href="/">zobacz więcej</Link>
                   </div>
                 </div>
               </div>
             </div>
 
-            <div className="grid grid-cols-[auto_1fr] h-full">
+            <div className="grid grid-cols-[0.4fr_0.6fr] md:grid-cols-[auto_1fr] h-full">
               <Link href="/" className="articleLink h-full">
                 <div className="article_secondImage relative h-full">
                   <Image
@@ -92,11 +92,11 @@ const News = () => {
                   </span>
                   <span className="text-[12px]">28.12.2023</span>
                 </div>
-                <div className="text-[#31404b]  mt-4 font-bold text-lg">
+                <div className="text-[#31404b]  mt-4 mb-4 font-bold text-sm sm:text-lg ">
                   Jesteśmy mistrzami IV ligi KNBA 🔥🔥🔥
                 </div>
                 <div className="mt-auto">
-                  <div className="bg-[#272727] px-4 py-2 font-bold rounded-md">
+                  <div className="bg-[#272727] px-4 py-2 font-bold rounded-md text-sm md:text-base">
                     <Link href="/">zobacz więcej</Link>
                   </div>
                 </div>

38e24fef086c68e7ae3fc2f1027c7137ce7d7124
Pawel Rosiek <rosiu978@gmail.com>
2024-02-10 22:20:16 +0100

    changing theme color for mobile to black

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff"></meta>
+        <meta name="theme-color" content="#000000" />
       </head>
       <body className={redHatFont.className}>{children}</body>
     </html>
